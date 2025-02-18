233a0a69e3de3ab6e9745d8f0f5e64f8f37cb2b9
yanzhi <yanzhi@enso-ai.com>
2025-02-18 12:47:13 -0800

    aws related scripts

---
+++ .aws/build_stack.sh
@@ -0,0 +1,9 @@
+aws cloudformation create-stack \
+  --stack-name tiktok-manager-internal \
+  --template-body file://s3_cloudfront.yaml \
+  --parameters \
+    ParameterKey=BucketName,ParameterValue=tiktok-manager-internal \
+    ParameterKey=DomainName,ParameterValue=tiktok.v01s.com \
+    ParameterKey=AcmCertificateArn,ParameterValue=arn:aws:acm:us-east-1:667865420292:certificate/f98e29c1-550c-4b9c-b738-2dd78aa4738b \
+  --capabilities CAPABILITY_NAMED_IAM
+
