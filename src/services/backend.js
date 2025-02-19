b382fa1c47c72b633277f45cc4f32bf65600c4e1
yanzhi <yanzhi@enso-ai.com>
2025-02-19 01:53:08 -0800

    Add new endpoint for download and upload file to backend

--- src/services/backend.js
+++ src/services/backend.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'https://tiktok-backend.v01s.com/api/';
-// const BASE_URL = 'http://127.0.0.1:8000/api/';
+// const BASE_URL = 'https://tiktok-backend.v01s.com/api/';
+const BASE_URL = 'http://127.0.0.1:8000/api/';
 
 export const verifyTiktokToken = async (code) => {
     const data = {
@@ -16,7 +16,7 @@ export const verifyTiktokToken = async (code) => {
 
     const json = await response.json();
     return json;
-}
+};
 
 export const listChannels = async () => {
     // todo pull a list of token
@@ -26,9 +26,41 @@ export const listChannels = async () => {
     });
 
     const url = BASE_URL + 'channels/?' + params.toString();
-    console.log("show url: ", url);
+    console.log('show url: ', url);
     const response = await fetch(url, { method: 'GET', mode: 'cors' });
 
     const json = await response.json();
     return json;
-}
+};
+
+export const downloadChannels = async () => {
+    // return file blob
+    const response = await fetch(BASE_URL + 'channels/export', {
+        method: 'GET',
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to download file');
+    }
+
+    // Create a downloadable link
+    const blob = await response.blob();
+    return blob;
+};
+
+export const uploadChannels = async (file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const response = await fetch(BASE_URL + 'channels/import', {
+        method: 'POST',
+        body: formData,
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+        throw new Error(data.error || 'Failed to upload file');
+    }
+
+    return data;
+};
