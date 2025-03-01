1229f56ee0e308f2e77835319a2b0e2adb0fef37
yanzhi <yanzhi@enso-ai.com>
2025-03-01 04:05:37 -0800

    update url to chimera.v01s.com

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 // this is sandbox account
 export const CLIENT_KEY = 'sbaw8h6p58f0t5wmyc';
-export const REDIRECT_URI = 'https://tiktok.v01s.com/login_callback/';
+export const REDIRECT_URI = 'https://chimera.v01s.com/login_callback/';
