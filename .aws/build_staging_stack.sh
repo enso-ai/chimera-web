02143e88b2dfa8c84e98c31a2c40ff2090b86f56
yanzhi <yanzhi@enso-ai.com>
2025-04-02 18:57:29 -0700

    update aws config

--- .aws/build_staging_stack.sh
+++ .aws/build_staging_stack.sh
@@ -3,7 +3,7 @@ aws cloudformation create-stack \
   --template-body file://s3_cloudfront.yaml \
   --parameters \
     ParameterKey=BucketName,ParameterValue=tiktok-manager-internal \
-    ParameterKey=DomainName,ParameterValue=chimera.v01s.com \
+    ParameterKey=DomainName,ParameterValue=chimera-staging.v01s.com \
     ParameterKey=AcmCertificateArn,ParameterValue=arn:aws:acm:us-east-1:667865420292:certificate/f98e29c1-550c-4b9c-b738-2dd78aa4738b \
   --capabilities CAPABILITY_NAMED_IAM
 
