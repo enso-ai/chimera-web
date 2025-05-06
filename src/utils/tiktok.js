934f7a930b8c251b7d8f2bbfa4c6c2fa81dbb0f6
yanzhi <yanzhi@enso-ai.com>
2025-05-06 16:41:59 -0700

    adjust dynamic scope when authenticate tiktok account

--- src/utils/tiktok.js
+++ src/utils/tiktok.js
@@ -1,9 +1,12 @@
 // util function related to tiktok
 import { CLIENT_KEY, REDIRECT_PATH } from 'constants';
 
-export const redirectToTiktokSignin = () => {
+export const redirectToTiktokSignin = (withPosting = false) => {
     const csrfState = Math.random().toString(36).substring(2); // Generate CSRF state
-    const scope = 'user.info.basic,user.info.stats,user.info.profile,video.list'; // Default scope
+    const scope = 'user.info.basic,user.info.stats,user.info.profile,video.list'; // scope without posting
+    if (withPosting) {
+        scope = 'user.info.basic,user.info.stats,user.info.profile,video.list,video.publish'; // scope with posting
+    }
 
     // Store csrfState in localStorage.  This is client-side storage, but it's
     // temporary and only for this initial redirect.  A more robust approach
@@ -13,7 +16,7 @@ export const redirectToTiktokSignin = () => {
     let url = 'https://www.tiktok.com/v2/auth/authorize/';
     // Template literals for cleaner string construction and encode all components
     url += `?client_key=${encodeURIComponent(CLIENT_KEY)}`;
-    url += `&scope=${encodeURIComponent(scope || 'user.info.basic')}`; // Default scope, encoded
+    url += `&scope=${encodeURIComponent(scope)}`; // Default scope, encoded
     url += '&response_type=code';
     // URL-encode the redirect URI!
     const redirectURL = window.location.origin + REDIRECT_PATH;
