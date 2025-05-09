dab13281604a154b4f3308a7130b274f35d63fa4
Jimmy <yanzhi@enso-ai.com>
2025-05-09 00:09:07 -0700

    fix typo

--- src/utils/tiktok.js
+++ src/utils/tiktok.js
@@ -3,7 +3,7 @@ import { CLIENT_KEY, REDIRECT_PATH } from 'constants';
 
 export const redirectToTiktokSignin = (withPosting = false) => {
     const csrfState = Math.random().toString(36).substring(2); // Generate CSRF state
-    const scope = 'user.info.basic,user.info.stats,user.info.profile,video.list'; // scope without posting
+    let scope = 'user.info.basic,user.info.stats,user.info.profile,video.list'; // scope without posting
     if (withPosting) {
         scope = 'user.info.basic,user.info.stats,user.info.profile,video.list,video.publish'; // scope with posting
     }
